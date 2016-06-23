f2fd363ffc9f9d2e09efeb4b7ee4135dafe98fa7
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2016-06-24 00:37:45 +0300

    Обновление зависимостей

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@
 //=include ../bower_components/promise-polyfill/promise.js
 //=include ../bower_components/zepto/zepto.js
 //=include ../bower_components/zepto-common/dist/zepto-common.js
+//=include ../bower_components/zepto-promise/dist/zepto-promise.js

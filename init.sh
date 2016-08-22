bfa6416e50b9d8afad1e436d3678d8c30b91f84b
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2016-08-22 13:55:11 +0300

    Обновление скрипта инициализации

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-[ $1 ]; sed "s/project_name/${1}/g" *.* > /dev/null
-bower install
-npm install
+if [ $1 ]; then
+  sed "s/project_name/${1}/g" *.* > /dev/null
+  bower install
+  npm install
+else
+  echo "Error: Enter the project_name!"
+  echo "Usage: ./init.sh project_name"
+fi
